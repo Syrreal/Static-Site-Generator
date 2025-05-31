f2f66882aa3152f1c74c8994b3807f5934618f30
Syrreal <SDHeil4561@gmail.com>
2025-05-30 18:20:40 -0700

    Added node splitting for inline elements & testing

--- src/main.py
+++ src/main.py
@@ -1,6 +1,34 @@
 from textnode import *
 from htmlnode import *
 
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    new_nodes = []
+    for node in old_nodes:
+        # TEXT texttype does not have a delimiter and does not need to be split
+        if node.text_type is not TextType.TEXT:
+            new_nodes.append(node)
+        else:
+            # Split with max of 2 splits
+            # If text has opening and closing delimiters we should get a 3 element list
+            text = node.text.split(delimiter, 2)
+            if len(text) == 2:
+                raise SyntaxError(f'Missing closing delimiter: {node}')
+            elif len(text) == 1:
+                # If only 1 element in list that means there are no delimiters left
+                # We can return the rest of the text as is
+                new_nodes.append(TextNode(text[0], TextType.TEXT))
+            else:
+                new_nodes.extend([
+                    TextNode(text[0], TextType.TEXT), 
+                    TextNode(text[1], text_type)
+                    ])
+                # Call the function again on the remaining text to check for more delimiters
+                new_nodes.extend(split_nodes_delimiter([TextNode(text[-1], TextType.TEXT)], delimiter, text_type))
+    # Filter out empty nodes in case a delimiter was the first or last character in the original text
+    filtered_nodes = [node for node in new_nodes if node.text]
+    return filtered_nodes
+
+
 def text_node_to_html_node(text_node):
     if text_node.text_type == TextType.BOLD:
         return LeafNode("b", text_node.text)
--- src/test_helpers.py
+++ src/test_helpers.py
@@ -2,7 +2,7 @@ import unittest
 
 from htmlnode import *
 from textnode import *
-from main import text_node_to_html_node
+from main import text_node_to_html_node, split_nodes_delimiter
 
 
 class TestTextToHTML(unittest.TestCase):
@@ -15,36 +15,76 @@ class TestTextToHTML(unittest.TestCase):
     
     def test_bold(self):
         node = TextNode("This is bold text", TextType.BOLD)
-        html_node =text_node_to_html_node(node)
+        html_node = text_node_to_html_node(node)
         self.assertEqual(html_node.tag, "b")
         self.assertEqual(html_node.value, "This is bold text")
     
     def test_italic(self):
         node = TextNode("This is italic text", TextType.ITALIC)
-        html_node =text_node_to_html_node(node)
+        html_node = text_node_to_html_node(node)
         self.assertEqual(html_node.tag, "i")
         self.assertEqual(html_node.value, "This is italic text")
     
     def test_code(self):
         node = TextNode("This is code text", TextType.CODE)
-        html_node =text_node_to_html_node(node)
+        html_node = text_node_to_html_node(node)
         self.assertEqual(html_node.tag, "code")
         self.assertEqual(html_node.value, "This is code text")
 
     def test_link(self):
         node = TextNode("This is a link", TextType.LINK, "https://www.google.com")
-        html_node =text_node_to_html_node(node)
+        html_node = text_node_to_html_node(node)
         self.assertEqual(html_node.tag, "a")
         self.assertEqual(html_node.value, "This is a link")
         self.assertEqual(html_node.props, {"href": "https://www.google.com"})
     
     def test_image(self):
         node = TextNode("This is an image", TextType.IMAGE, "img.src")
-        html_node =text_node_to_html_node(node)
+        html_node = text_node_to_html_node(node)
         self.assertEqual(html_node.tag, "img")
         self.assertEqual(html_node.value, None)
         self.assertEqual(html_node.props, {"src": "img.src", "alt": "This is an image"})
+    
+    def test_empty_bold(self):
+        node = TextNode("", TextType.BOLD)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "b")
 
+class TestSplitNodesDelimiter(unittest.TestCase):
+    
+    # def test_single_block(self):
+    #     starting_nodes = [TextNode("Text with 'code' block", TextType.TEXT)]
+    #     new_nodes = split_nodes_delimiter(starting_nodes, "'", TextType.CODE)
+    #     self.assertEqual(new_nodes, [
+    #                             TextNode('Text with ', TextType.TEXT), 
+    #                             TextNode('code', TextType.CODE), 
+    #                             TextNode(' block', TextType.TEXT)
+    #                             ])
+
+    def test_sequential_blocks(self):
+        starting_nodes = [TextNode("Text with _two__italic_ words", TextType.TEXT)]
+        new_nodes = split_nodes_delimiter(starting_nodes, "_", TextType.ITALIC)
+        self.assertEqual(new_nodes, [
+                                TextNode('Text with ', TextType.TEXT), 
+                                TextNode('two', TextType.ITALIC),
+                                TextNode('italic', TextType.ITALIC), 
+                                TextNode(' words', TextType.TEXT)
+                                ])
+        
+    # def test_non_sequential_blocks(self):
+    #     starting_nodes = [TextNode("Text with **multiple** bold **lines** that arent right next to eachother", TextType.TEXT)]
+    #     new_nodes = split_nodes_delimiter(starting_nodes, "**", TextType.BOLD)
+    #     self.assertEqual(new_nodes, [
+    #                             TextNode('Text with ', TextType.TEXT), 
+    #                             TextNode('multiple', TextType.BOLD),
+    #                             TextNode(' bold ', TextType.TEXT),
+    #                             TextNode('lines', TextType.BOLD), 
+    #                             TextNode(' that arent right next to eachother', TextType.TEXT)
+    #                             ])
+        
+    # def test_incomplete_blocks(self):
+    #     starting_nodes = [TextNode("Text with an incomplete 'code block", TextType.TEXT)]
+    #     self.assertRaises(SyntaxError, split_nodes_delimiter, starting_nodes, "'", TextType.CODE)
 
 if __name__ == "__main__":
     unittest.main()
