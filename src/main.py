dadc4f1cd00f799c67f54dbb378bde91c44c71eb
Syrreal <SDHeil4561@gmail.com>
2025-06-01 16:08:29 -0700

    Added raw text to text node conversion function and testing

--- src/main.py
+++ src/main.py
@@ -117,6 +117,18 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
     filtered_nodes = [node for node in new_nodes if node.text]
     return filtered_nodes
 
+def text_to_text_nodes(text):
+    text_nodes = [TextNode(text, TextType.TEXT)]
+    delimiters_types = [
+        ("**", TextType.BOLD),
+        ("_", TextType.ITALIC),
+        ("`", TextType.CODE),
+        ]
+    new_nodes = []
+    for delimiter, text_type in delimiters_types:
+        text_nodes = split_nodes_delimiter(text_nodes, delimiter, text_type)
+    text_nodes = split_nodes_image(text_nodes)
+    return split_nodes_image(text_nodes)
 
 def text_node_to_html_node(text_node):
     if text_node.text_type == TextType.BOLD:
@@ -131,6 +143,7 @@ def text_node_to_html_node(text_node):
         return LeafNode("a", text_node.text, {"href": text_node.url})
     if text_node.text_type == TextType.TEXT:
         return LeafNode(None, text_node.text)
+
 
 def main():
     a = TextNode("achor text", TextType.LINK, "https://www.google.com")
