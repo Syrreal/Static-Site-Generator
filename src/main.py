d5030ce26e6ebf429d21d86bdcf038c74ed71279
Syrreal <SDHeil4561@gmail.com>
2025-06-02 22:06:45 -0700

    Added markdown block splitting function and testing

--- src/main.py
+++ src/main.py
@@ -29,6 +29,9 @@ def text_node_to_html_node(text_node):
     if text_node.text_type == TextType.TEXT:
         return LeafNode(None, text_node.text)
 
+def markdown_to_blocks(markdown):
+    return list(map(lambda x: x.strip(), markdown.split("\n\n")))
+
 def main():
     a = TextNode("achor text", TextType.LINK, "https://www.google.com")
     b = TextNode("bold text", TextType.BOLD)
---
+++ src/test_markdowntoblocks.py
@@ -0,0 +1,74 @@
+import unittest
+
+from main import *
+
+class TestMarkdownToBlocks(unittest.TestCase):
+    
+    def test_heading(self):
+        md = """
+# This is **bolded** heading
+"""
+        blocks = markdown_to_blocks(md)
+        self.assertEqual(
+            blocks,
+            [
+                "# This is **bolded** heading",
+            ],
+        )
+
+    def test_list(self):
+        md = """
+- This is a list
+- with items
+"""
+        blocks = markdown_to_blocks(md)
+        self.assertEqual(
+            blocks,
+            [
+                "- This is a list\n- with items",
+            ],
+        )
+
+    def test_multiple_same(self):
+        md = """
+This is another paragraph with _italic_ text and `code` here
+This is the same paragraph on a new line
+
+This is a seperate paragraph
+that also has multiple lines
+"""
+        blocks = markdown_to_blocks(md)
+        self.assertEqual(
+            blocks,
+            [
+                "This is another paragraph with _italic_ text and `code` here\nThis is the same paragraph on a new line",
+                "This is a seperate paragraph\nthat also has multiple lines"
+            ],
+        )
+
+    def test_multiple(self):
+        md = """
+This is **bolded** paragraph
+
+This is another paragraph with _italic_ text and `code` here
+This is the same paragraph on a new line
+
+- This is a list
+- with items
+
+- This is another list
+- with more items
+"""
+        blocks = markdown_to_blocks(md)
+        self.assertEqual(
+            blocks,
+            [
+                "This is **bolded** paragraph",
+                "This is another paragraph with _italic_ text and `code` here\nThis is the same paragraph on a new line",
+                "- This is a list\n- with items",
+                "- This is another list\n- with more items"
+            ],
+        )
+
+if __name__ == "__main__":
+    unittest.main()
