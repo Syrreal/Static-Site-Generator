1fc07df5cf550364ea295c66c67dd3ac021dbf45
Syrreal <SDHeil4561@gmail.com>
2025-06-02 22:14:46 -0700

    Fixed bug causing text node splitting on links to fail

--- src/main.py
+++ src/main.py
@@ -13,7 +13,7 @@ def text_to_text_nodes(text):
     for delimiter, text_type in delimiters_types:
         text_nodes = split_nodes_delimiter(text_nodes, delimiter, text_type)
     text_nodes = split_nodes_image(text_nodes)
-    return split_nodes_image(text_nodes)
+    return split_nodes_link(text_nodes)
 
 def text_node_to_html_node(text_node):
     if text_node.text_type == TextType.BOLD:
