5831a13db38671a771aa7661ff9b4c6beae1350d
Syrreal <SDHeil4561@gmail.com>
2025-06-22 11:36:49 -0700

    Added html page generation

--- src/main.py
+++ src/main.py
@@ -1,7 +1,9 @@
-import os
 import shutil
 import pathlib
 import re
+import os
+
+from markdowntohtml import markdown_to_html_node
 
 def move_children(source, dest):
     print(f'Checking for children in: {source}')
@@ -35,8 +37,9 @@ def copy_source_to_dest(source, dest):
     os.mkdir(dest)
 
     # Call recursive function to move tree
-    print("Moving tree -----------")
+    print("Moving tree ------------------")
     move_children(source, dest)
+    print("Finished ---------------------")
 
 def extract_title(markdown):
     title = re.findall(r"(?<!#)#{1}(?!#)[^#].*", markdown)
@@ -44,8 +47,34 @@ def extract_title(markdown):
         raise ValueError("Markdown missing h1 header")
     return title[0].removeprefix("#").strip()
 
+def generate_page(from_path, template_path, dest_path):
+    print(f'Generating page from "{from_path}" to "{dest_path}" using "{template_path}"')
+    md = None
+    template = None
+    with open(from_path, "+r") as f:
+        md = f.read()
+    with open(template_path, "+r") as f:
+        template = f.read()
+    
+    title = extract_title(md)
+    html = markdown_to_html_node(md).to_html()
+    template = template.replace("{{ Title }}", title)
+    template = template.replace("{{ Content }}", html)
+    print("Creating destination path")
+    # Remove file from dest_path before creating parent directory(s)
+    # re.split leaves an empty string in this case so we filter it out before assigning variables
+    parent_path, filename = filter(None, re.split(r"\/([\w\_\-\s]*\.[\w]*)", dest_path))
+
+    pathlib.Path(parent_path).mkdir(parents=True, exist_ok=True)
+    print("Writing content to dest")
+    with open(dest_path, "+x") as dest:
+        # write our overridden template file to destination
+        dest.write(template)
+    print("Finished -----------------------")
+
 def main():
-    pass
+    copy_source_to_dest("./static", "./public")
+    generate_page("./content/index.md", "./template.html", "./public/index.html")
 
 if __name__ == "__main__":
     main()
